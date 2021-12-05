a9a37e6097c47542c341d99230d22fda578961ad
Adkhambek <mr.adkhambek@gmail.com>
2021-12-05 18:50:45 +0500

    add SingleLiveData.kt

--- app/src/main/java/r2/llc/referee/main/mvp/MainContract.kt
+++ app/src/main/java/r2/llc/referee/main/mvp/MainContract.kt
@@ -1,26 +1,7 @@
 package r2.llc.referee.main.mvp
 
-import r2.llc.referee.base.BasePresenter
-
 interface MainContract {
 
-    interface View {
-
-        fun updateScore(model: Model)
-        fun updateTimer(time: Long)
-
-        fun showResult(model: Model)
-        fun onChangeGameStatus(model: Model)
-    }
-
-    interface Presenter: BasePresenter<View> {
-
-        fun onGoalTop()
-        fun onGoalBottom()
-
-        fun onStart()
-        fun onReset()
-    }
 
     interface Model {
         val topScore: Long
